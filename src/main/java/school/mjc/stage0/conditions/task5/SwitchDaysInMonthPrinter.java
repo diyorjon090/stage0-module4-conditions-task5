8105876369f131d81ebf919444da5acef650bf0f
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-11-24 05:04:19 +0500

    module4.1 task5

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month){
+            case 12:
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                System.out.println("31");
+                break;
+            case 2:
+                System.out.println("28");
+            case 11:
+            case 4:
+            case 6:
+            case 9:
+                System.out.println("30");
+                break;
+        }
     }
 }
